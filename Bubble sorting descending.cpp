2f3390bc306d8645e02f426c01d3d1d3293a468c
Nishat Yeasmin <nishatyasminnisha@gmail.com>
2025-10-06 23:17:36 +0600

    Add files via upload

---
+++ Bubble sorting descending.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i;
+    cout<<"Enter array size: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter array elements: ";
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int counter = 1;
+    while(counter<n)
+    {
+        for(i=0;i<n-counter;i++)
+        {
+            if(arr[i]<arr[i+1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+        counter++;
+    }
+    cout<<"Sorted bubble array element: ";
+    for(i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
